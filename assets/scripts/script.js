c33b6893184926deb4dbe6d1c6971d57d2e35a61
Andrew Provis <aprovis@dresources.com>
2015-04-30 12:20:00 +0100

    Added function to add items from input field to In Progress list.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,11 @@
  * Created by aprovis on 29/04/2015.
  */
 
+/**
+ * jQuery document.ready runs once the page has loaded.
+ *
+ * I'm using this to bind my functionality to initial elements on the screen.
+ */
 $(function() {
     console.log( "ready!" );
 
@@ -9,10 +14,29 @@ $(function() {
 });
 
 
-
+/**
+ * Adds item to the In Progress list from the input field at the top. Will only add an item if there is one to enter.
+ * @param event
+ */
 function addItem(event) {
-    console.log(arguments);
+    //console.log(arguments);
+
+    // Checks of there is any text in the input and if not then the function will exit.
+    if (document.getElementsByName('todo-input')[0].value === '') {
+        return;
+    }
+
+    // Adds to-do item to the list.
+    $('#InProgress').append(
+        '<li class="todo-item">' +
+            '<div class="item-text">' + document.getElementsByName('todo-input')[0].value + '<span class="edit-button"></span><span class="completion-button"></span></div>' +
+        '</li>'
+    );
+
+    // Resetting the input field at the top ready for anything else to be entered.
+    document.getElementsByName('todo-input')[0].value = '';
 }
+
 
 function deleteItem() {
 

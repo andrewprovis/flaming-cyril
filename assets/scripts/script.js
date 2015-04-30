db8ae7313b5ebf22d4ab5e33f518a03be84c53d5
Andrew Provis <aprovis@dresources.com>
2015-04-30 09:38:32 +0100

    Added a page structure and styled. Should be ready to add functionality.

---
+++ assets/scripts/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by aprovis on 29/04/2015.
+ */
+
+$(function() {
+    console.log( "ready!" );
+
+    $(".add-button").on("click", addItem);
+});
+
+
+
+function addItem(event) {
+    console.log(arguments);
+}
+
+function deleteItem() {
+
+}

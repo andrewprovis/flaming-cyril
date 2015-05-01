88a8f110b5c2ba75d91cedf00eecd920ceb66c27
Andrew Provis <aprovis@dresources.com>
2015-05-01 16:41:44 +0100

    Added create item function to handle adding items from the input and modified the addNewItem function in preparation for adding items from local storage.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,11 +8,11 @@
  * I'm using this to bind my functionality to initial elements on the screen.
  */
 $(function() {
-    $(".add-button").on("click", addNewItem);
+    $(".add-button").on("click", createNewItem);
 
     $('#todo-input').keypress(function (e) {
         if (e.which == 13) {
-            addNewItem();
+            createNewItem();
             return false;
         }
     });
@@ -26,26 +26,36 @@ $(function() {
  * Adds item to the In Progress list from the input field at the top. Will only add an item if there is one to enter.
  * @param event
  */
-function addNewItem(event) {
-    //console.log(arguments);
+function loadItems() {
 
+}
+
+function createNewItem() {
     // Checks of there is any text in the input and if not then the function will exit.
     if (document.getElementsByName('todo-input')[0].value === '') {
         return;
     }
 
+    addNewItem(document.getElementsByName('todo-input')[0].value);
+
+    // Resetting the input field at the top ready for anything else to be entered.
+    document.getElementsByName('todo-input')[0].value = '';
+}
+
+/**
+ * Adds item to the In Progress list from the input field at the top. Will only add an item if there is one to enter.
+ * @param event
+ */
+function addNewItem(itemText) {
     // Adds to-do item to the list.
     $('#ToDo').append(
         '<li class="todo-item">' +
             '<div class="item-text">' +
-                document.getElementsByName('todo-input')[0].value +
-                '<span class="completion-button"></span><span class="delete-button"></span>' +
+                itemText +
+                '<span class="completion-button"></span><span class="delete-button"></span><span class="edit-button"></span>' +
             '</div>' +
         '</li>'
     );
-
-    // Resetting the input field at the top ready for anything else to be entered.
-    document.getElementsByName('todo-input')[0].value = '';
 }
 
 function closeItem() {
